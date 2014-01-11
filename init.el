37dcf8a1a9c80650d9cf50b76c6d68994ee763d4
Matthew Gordon <matthew.scott.gordon@gmail.com>
2014-01-11 00:05:36 -0400

    Change keyboard shortcut for goto-line.

--- init.el
+++ init.el
@@ -129,7 +129,7 @@
 (global-unset-key (kbd "s-o"))
 (global-set-key (kbd "s-o") 'ff-find-other-file)
 (global-set-key (kbd "s-r") 'revert-buffer)
-(global-set-key (kbd "s-g") 'goto-line)
+(global-set-key (kbd "s-l") 'goto-line)
 
 ;;Unicode
 (global-set-key (kbd "C-c u -") (lambda () (interactive) (ucs-insert "2013"))) ;;en-dash
