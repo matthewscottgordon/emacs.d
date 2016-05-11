60e2469acb21efbb027b2a8f9b022ff0d263f333
Matthew Gordon <matthew.scott.gordon@gmail.com>
2016-05-11 10:19:01 -0300

    Enable line numbers for Python source

--- init.el
+++ init.el
@@ -100,6 +100,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-hook 'python-mode-hook '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
+(add-hook 'python-mode-hook (lambda () (linum-mode 1)))
 
 
 (custom-set-variables
