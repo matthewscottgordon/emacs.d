f388438f5d0ebb8f27416415139cd8b608d719cd
Matthew Gordon <matthew@gordon.earth>
2021-01-07 11:11:32 -0500

    Add ace-window package

--- init.el
+++ init.el
@@ -48,6 +48,9 @@
 (add-hook 'term-mode-hook (lambda ()
                             (setq-local show-trailing-whitespace nil)))
 
+(use-package ace-window)
+(global-set-key (kbd "M-o") 'ace-window)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fonts and colors
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
